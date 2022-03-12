eea2c8a59d6044285cd76fdb8cd76c6b380f5926
ttejeda <ttejeda@ucol.mx>
2022-03-12 16:42:53 -0600

    part 2.5, update comments

--- events/app.js
+++ events/app.js
@@ -10,13 +10,13 @@ let obj = {
     }
 }
 
-console.log(obj.nombre); //Pregunta por el nombre de obj
-console.log(obj['nombre']); //Pregunta por la posición de nombre dentro de obj
+console.log(obj.nombre); // Pregunta por el nombre de obj
+console.log(obj['nombre']); // Pregunta por la posición de nombre dentro de obj
 
-let myKey = 'nombre'; //Utilizamos una 'key' para cambiar de manera fácil identificar lo que se quiere obtener
-console.log(obj[myKey]); //Da el mismo resultado que las anteriores llamadas, debido a que "myKey" tiene el valor de nombre
+let myKey = 'nombre'; // Utilizamos una 'key' para cambiar de manera fácil identificar lo que se quiere obtener
+console.log(obj[myKey]); // Da el mismo resultado que las anteriores llamadas, debido a que "myKey" tiene el valor de nombre
 
 myKey = 'keypress';
-console.log(obj[myKey]());
+console.log(obj[myKey]()); // La función de keypress se ejecuta y muestra el resultado de la consola, pero al no devolver nada, da "undefined" y no se imprime este "console.log"
 myKey = 'mouseOver';
-console.log(obj[myKey]());
+console.log(obj[myKey]()); // La función mouseOver se ejecuta, pero al no regresar ningún valor, el "console.log" da "undefined"
