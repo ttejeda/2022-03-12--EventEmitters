57e7e732418c7fb45eb7b306b00b3c6bc0b403ff
ttejeda <ttejeda@ucol.mx>
2022-03-12 16:41:20 -0600

    part 2, keypress and mouseOver

--- events/app.js
+++ events/app.js
@@ -1,7 +1,13 @@
 let obj = {
     nombre: 'Tirzo',
     edad: 20,
-    apellido: 'Tejeda'
+    apellido: 'Tejeda',
+    keypress: function (){
+        console.log(`Se ha presionado una tecla`);
+    },
+    mouseOver: function (){
+        console.log(`El puntero del ratón está arriba`);
+    }
 }
 
 console.log(obj.nombre); //Pregunta por el nombre de obj
@@ -9,3 +15,8 @@ console.log(obj['nombre']); //Pregunta por la posición de nombre dentro de obj
 
 let myKey = 'nombre'; //Utilizamos una 'key' para cambiar de manera fácil identificar lo que se quiere obtener
 console.log(obj[myKey]); //Da el mismo resultado que las anteriores llamadas, debido a que "myKey" tiene el valor de nombre
+
+myKey = 'keypress';
+console.log(obj[myKey]());
+myKey = 'mouseOver';
+console.log(obj[myKey]());
